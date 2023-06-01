977f0584dff439d5569fb314aa6d5a6d302bea33
EC2 Default User <ec2-user@ip-172-31-25-183.ec2.internal>
2023-06-01 01:06:43 +0000

    new

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from my_pipeline.my_pipeline_stack import MyPipelineStack
+from my_pipeline.my_pipeline_stack_new import MyPipelineStack
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack",
